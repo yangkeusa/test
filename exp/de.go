49da37eac623ea40f9a12cdadb55973fec414414
Ke Yang <yangke@google.com>
2015-04-06 11:40:48 -0700

    formating change

--- exp/de.go
+++ exp/de.go
@@ -5,17 +5,17 @@ package exp
 import "log"
 
 func A(log *log.Logger, x int) {
-	log.Printf("A: x = %d\n", x)
+	log.Printf("A: x = %v\n", x)
 }
 
 func B(log *log.Logger, y int) {
 	defer A(log, y+1)
 	defer A(log, y+10)
-	log.Printf("B: y = %d\n", y)
+	log.Printf("B: y = %v\n", y)
 }
 
 func C(log *log.Logger, z int) {
 	defer A(log, z+1)
 	defer B(log, z+10)
-        log.Printf("C: z = %d\n", z)
+        log.Printf("C: z = %v\n", z)
 }
